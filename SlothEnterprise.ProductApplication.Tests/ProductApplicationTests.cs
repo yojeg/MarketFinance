0c9389e911af2797046eb77bc2079e010e49f699
ulrihiv <ulrihiv@gmail.com>
2020-11-22 18:59:27 +0500

    test for selectInvoice service call is added

--- SlothEnterprise.ProductApplication.Tests/ProductApplicationTests.cs
+++ SlothEnterprise.ProductApplication.Tests/ProductApplicationTests.cs
@@ -1,13 +1,38 @@
-﻿using Xunit;
+﻿using Moq;
+using SlothEnterprise.External.V1;
+using SlothEnterprise.ProductApplication.Applications;
+using SlothEnterprise.ProductApplication.Products;
+using Xunit;
 
 namespace SlothEnterprise.ProductApplication.Tests
 {
     public class ProductApplicationTests
     {
-        [Fact]
-        public void ThisIsTrue()
+        private readonly Mock<ISelectInvoiceService> _selectInvoiceServiceMock;
+        private Mock<IBusinessLoansService> _businessLoansServiceMock;
+        private Mock<IConfidentialInvoiceService> _confidentialInvoiceServiceMock;
+
+        public ProductApplicationTests()
         {
-            Assert.Equal(1, 1);
+            _selectInvoiceServiceMock = new Mock<ISelectInvoiceService>();
+            _businessLoansServiceMock = new Mock<IBusinessLoansService>();
+            _confidentialInvoiceServiceMock = new Mock<IConfidentialInvoiceService>();
+        }
+
+        [Fact]
+        public void ProductApplicationService_ShouldSubmitApplication_ToSelectInvoiceService()
+        {
+            var productApplicationService = new ProductApplicationService(_selectInvoiceServiceMock.Object, null, null);
+
+            productApplicationService.SubmitApplicationFor(new SellerApplication()
+            {
+                Product = new SelectiveInvoiceDiscount(),
+                CompanyData = new SellerCompanyData()
+                {
+                }
+            });
+
+            _selectInvoiceServiceMock.Verify(x => x.SubmitApplicationFor(It.IsAny<string>(), It.IsAny<decimal>(), It.IsAny<decimal>()), Times.Once);
         }
     }
 }
